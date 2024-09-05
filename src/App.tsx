3bd8f5f7607cb7ddeb1aea7b27bc9e005c598d38
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-05 09:38:01 -0300

    Configure React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router"
+
 function App() {
   return (
     <>
-
+      <Outlet/>
     </>
   )
 }
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () =>{
+    return(
+        <div>
+            <h1>Testando Router Dom!</h1>
+        </div>     
+    )
+}
+
+export default Home;
