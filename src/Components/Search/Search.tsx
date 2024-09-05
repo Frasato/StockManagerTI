460be04e2cfeaebbb10c21e72cee473507f81e43
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-05 11:46:05 -0300

    Add margin bottom spacing

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -2,8 +2,7 @@ import { FaSearch } from "react-icons/fa";
 
 const Search = () =>{
     return(
-        <section className="flex flex-col justify-center items-center">
-            <h1 className="text-4xl text-neutral-50 font-bold mb-5">Stock Itens</h1>
+        <section className="flex flex-col justify-center items-center mb-10">
             <div className="flex items-center justify-between border-2 rounded-xl w-5/12 p-1">
                 <input type="text" placeholder="Barcode... Name... Mark..." className="bg-transparent w-11/12 focus:outline-none text-neutral-50"/>
                 <FaSearch className="text-neutral-50 mx-3 hover:cursor-pointer hover:text-orange-600"/>
