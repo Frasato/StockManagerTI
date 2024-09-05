a752ee31bad733ec8e0d52c3a07ac0ba0100ebe4
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-05 10:55:47 -0300

    Create styles

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -2,11 +2,11 @@ import { FaSearch } from "react-icons/fa";
 
 const Search = () =>{
     return(
-        <section>
-            <h1 className="text-lg text-neutral-50 font-bold">Stock Itens</h1>
-            <div>
-                <input type="text" placeholder="Barcode... Name... Mark..."/>
-                <FaSearch />
+        <section className="flex flex-col justify-center items-center">
+            <h1 className="text-4xl text-neutral-50 font-bold mb-5">Stock Itens</h1>
+            <div className="flex items-center justify-between border-2 rounded-xl w-5/12 p-1">
+                <input type="text" placeholder="Barcode... Name... Mark..." className="bg-transparent w-11/12 focus:outline-none text-neutral-50"/>
+                <FaSearch className="text-neutral-50 mx-3 hover:cursor-pointer hover:text-orange-600"/>
             </div>
         </section>
     )
