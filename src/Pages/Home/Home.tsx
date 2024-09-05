c4032f53934ef38f6e58394c66cd9c1cbfe30c7b
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-05 11:46:18 -0300

    Add List and title for page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import List from "../../Components/List/List";
 import NavBar from "../../Components/NavBar/NavBar";
 import Search from "../../Components/Search/Search";
 
@@ -5,7 +6,11 @@ const Home = () =>{
     return(
         <main>
             <NavBar />
+            <div className="flex justify-center">
+                <h1 className="text-4xl text-neutral-50 font-bold mb-5">Stock Itens</h1>
+            </div>
             <Search />
+            <List />
         </main>     
     )
 }
