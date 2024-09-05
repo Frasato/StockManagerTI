f1bfb36a0bfaff77ed36f8ab4afff4d0bccaad13
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-05 11:45:38 -0300

    Create List component

---
+++ src/Components/List/List.tsx
@@ -0,0 +1,15 @@
+const List = () =>{
+    return(
+        <section className="bg-gray-800 h-10">
+                <ul className="flex justify-around items-center h-10">
+                    <li className="text-neutral-50">0000000000000</li>
+                    <li className="text-neutral-50">Font PC</li>
+                    <li className="text-neutral-50">Microsoft</li>
+                    <li className="text-neutral-50">12</li>
+                    <li className="text-neutral-50">12/01/2026</li>
+                </ul>
+        </section>
+    )
+}
+
+export default List;
