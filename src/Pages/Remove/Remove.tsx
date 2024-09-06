42d46e5c5545050bb8fda7e1930c8ad668e92e88
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-06 11:23:17 -0300

    Create Remove page

---
+++ src/Pages/Remove/Remove.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../../Components/NavBar/NavBar";
+
+const Remove = () =>{
+    return(
+        <main>
+            <NavBar />
+        </main>
+    )
+}
+
+export default Remove;
