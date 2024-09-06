76e2e1f6cd261937ef693b78f7aab865d81786b6
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-06 15:38:30 -0300

    Redirect to Home when click logo

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import logo from "../../Assets/Licon.png";
+import { useState } from "react";
 
 const NavBar = () =>{
+
+    const [redirect, setRedirect] = useState<boolean>(false);
+
+    const goToHome = () => setRedirect(true);
+
+    if(redirect) return <Navigate to="/"/>
+
     return(
         <nav className="flex justify-around items-center p-10">
-            <img src={logo} alt="Logo TI" className="w-10"/>
+            <img src={logo} alt="Logo TI" className="w-10 cursor-pointer" onClick={goToHome}/>
             <div>
                 <Link to="/" className="text-neutral-50 mx-5 hover:text-neutral-300">HOME</Link>
                 <Link to="/out" className="text-neutral-50 mx-5 hover:text-neutral-300">OUT's</Link>
-                <Link to="/update" className="text-neutral-50 mx-5 hover:text-neutral-300">CHANGE</Link>
                 <Link to="/create" className="text-neutral-50 mx-5 bg-orange-600 p-2 rounded-md hover:bg-orange-500">ADD</Link>
                 <Link to="/remove" className="text-neutral-50 mx-5 bg-orange-600 p-2 rounded-md hover:bg-orange-500">TAKE OUT</Link>
             </div>
