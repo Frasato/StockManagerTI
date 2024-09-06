5dd391726457c29a635abe6e3776e8021dccf448
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-06 09:57:35 -0300

    Placeholder on input

--- src/Pages/Create/Create.tsx
+++ src/Pages/Create/Create.tsx
@@ -8,22 +8,22 @@ const Create = () =>{
             <section className="flex flex-col justify-center items-center min-h-full gap-5">
                 <div className="flex flex-col w-2/3">
                     <label className="text-neutral-50">Barcode</label>
-                    <input type="text" className="bg-transparent border-2 rounded-xl w-full h-10 text-neutral-50 focus:outline-none p-2"/>
+                    <input type="text" className="bg-transparent border-2 rounded-xl w-full h-10 text-neutral-50 focus:outline-none p-2" placeholder="7538239120000"/>
                 </div>
                 <div className="flex gap-5 items-center justify-center w-2/3">
                     <div className="flex flex-col w-2/4">
                         <label className="text-neutral-50">Product Name</label>
-                        <input type="text" className="bg-transparent border-2 rounded-xl h-10 text-neutral-50 focus:outline-none p-2"/>
+                        <input type="text" className="bg-transparent border-2 rounded-xl h-10 text-neutral-50 focus:outline-none p-2" placeholder="Notebook Aspire 5..."/>
                     </div>
                     <div className="flex flex-col w-2/4">
                         <label className="text-neutral-50">Product Mark</label>
-                        <input type="text" className="bg-transparent border-2 rounded-xl h-10 text-neutral-50 focus:outline-none p-2"/>
+                        <input type="text" className="bg-transparent border-2 rounded-xl h-10 text-neutral-50 focus:outline-none p-2" placeholder="Acer..."/>
                     </div>
                 </div>
                 <div className="flex gap-5 items-end w-2/3">
                     <div className="flex flex-col w-2/4">
                         <label className="text-neutral-50">Amount</label>
-                        <input type="text" className="bg-transparent border-2 rounded-xl h-10 text-neutral-50 focus:outline-none p-2"/>
+                        <input type="text" className="bg-transparent border-2 rounded-xl h-10 text-neutral-50 focus:outline-none p-2" placeholder="12"/>
                     </div>
                     <button className="bg-orange-600 text-neutral-50 h-10 w-2/4 rounded-xl hover:bg-orange-500">Create</button>
                 </div>
