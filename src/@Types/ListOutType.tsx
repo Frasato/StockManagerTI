dd19cf200c58b0de518fe4eab1f4f736ab841f48
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-06 11:24:30 -0300

    Create List Out Type

---
+++ src/@Types/ListOutType.tsx
@@ -0,0 +1,11 @@
+export type ListOutType = {
+    id?: string,
+    barCode: string,
+    itemName: string,
+    markName: string,
+    amount: string,
+    sectorName: string,
+    cityName: string,
+    outDate?: string,
+    color?: string,
+}
