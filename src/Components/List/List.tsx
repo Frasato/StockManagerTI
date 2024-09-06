900c608e13e1ed381de4e6ec30ea9e06043f74b0
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-06 08:27:30 -0300

    Create Props for List component

--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -1,12 +1,14 @@
-const List = () =>{
+import { ListType } from "../../@Types/ListType";
+
+const List = (Props: ListType) =>{
     return(
-        <section className="bg-gray-800 h-10">
+        <section className={Props.color+" h-10"} key={Props.id}>
                 <ul className="flex justify-around items-center h-10">
-                    <li className="text-neutral-50">0000000000000</li>
-                    <li className="text-neutral-50">Font PC</li>
-                    <li className="text-neutral-50">Microsoft</li>
-                    <li className="text-neutral-50">12</li>
-                    <li className="text-neutral-50">12/01/2026</li>
+                    <li className="text-neutral-50">{Props.barCode}</li>
+                    <li className="text-neutral-50">{Props.itemName}</li>
+                    <li className="text-neutral-50">{Props.markName}</li>
+                    <li className="text-neutral-50">{Props.amount}</li>
+                    <li className="text-neutral-50">{Props.createdDate}</li>
                 </ul>
         </section>
     )
