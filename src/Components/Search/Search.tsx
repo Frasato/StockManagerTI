523435594f736389dfb3913a6d8c475810d10121
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-06 15:38:10 -0300

    onChange Props

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,10 +1,14 @@
 import { FaSearch } from "react-icons/fa";
 
-const Search = () =>{
+type seacrhType = {
+    change?: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const Search = (Props: seacrhType) =>{
     return(
         <section className="flex flex-col justify-center items-center mb-10">
             <div className="flex items-center justify-between border-2 rounded-xl w-5/12 p-1">
-                <input type="text" placeholder="Barcode... Name... Mark..." className="bg-transparent w-11/12 focus:outline-none text-neutral-50"/>
+                <input type="text" placeholder="Barcode... Name... Mark..." className="bg-transparent w-11/12 focus:outline-none text-neutral-50" onChange={Props.change}/>
                 <FaSearch className="text-neutral-50 mx-3 hover:cursor-pointer hover:text-orange-600"/>
             </div>
         </section>
