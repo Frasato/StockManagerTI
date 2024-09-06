cfbe0d579f8f67cbefb50efaa252068d57c40352
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-06 08:25:30 -0300

    Calling API and rendering on List component

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
 import List from "../../Components/List/List";
 import NavBar from "../../Components/NavBar/NavBar";
 import Search from "../../Components/Search/Search";
+import { ListType } from "../../@Types/ListType";
 
 const Home = () =>{
+
+    const [itens, setItens] = useState<ListType[]>([]);
+
+    useEffect(()=>{
+        const getAll = async() =>{
+            try{
+                const response = await fetch("http://localhost:8080/itens/all");
+                
+                if(!response.ok){
+                    throw new Error("Failed to fetch items: " + response.statusText);
+                }
+
+                const responseData: ListType[] = await response.json();
+                setItens(responseData);
+            }catch(err){
+                console.error(err);
+            }
+        }
+
+        getAll();
+    },[itens])
+
     return(
         <main>
             <NavBar />
@@ -10,7 +34,11 @@ const Home = () =>{
                 <h1 className="text-4xl text-neutral-50 font-bold mb-5">Stock Itens</h1>
             </div>
             <Search />
-            <List />
+            {itens.map((item, index)=>{
+                return(
+                    <List barCode={item.barCode} itemName={item.itemName} markName={item.markName} amount={item.amount} createdDate={item.createdDate} color={index % 2 === 0? "bg-gray-600": ""} id={item.id} key={item.id}/>
+                )
+            })}
         </main>     
     )
 }
