749fb5e10d077785cd1d22ae8d1775fe991f60ea
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-09 10:25:31 -0300

    Change list style

--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -3,12 +3,12 @@ import { ListType } from "../../@Types/ListType";
 const List = (Props: ListType) =>{
     return(
         <section className={Props.color+" h-10"} key={Props.id}>
-                <ul className="flex justify-around items-center h-10">
-                    <li className="text-neutral-50">{Props.barCode}</li>
-                    <li className="text-neutral-50">{Props.itemName}</li>
-                    <li className="text-neutral-50">{Props.markName}</li>
-                    <li className="text-neutral-50">{Props.amount}</li>
-                    <li className="text-neutral-50">{Props.createdDate}</li>
+                <ul className="flex justify-start items-center h-10 ml-32">
+                    <li className="text-neutral-50 text-left flex-1">{Props.barCode}</li>
+                    <li className="text-neutral-50 text-left flex-1">{Props.itemName}</li>
+                    <li className="text-neutral-50 text-left flex-1">{Props.markName}</li>
+                    <li className="text-neutral-50 text-left flex-1">{Props.amount}</li>
+                    <li className="text-neutral-50 text-left flex-1">{Props.createdDate}</li>
                 </ul>
         </section>
     )
