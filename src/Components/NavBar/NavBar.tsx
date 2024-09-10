fbbe4c9a2383d3def852caf0fcd8cc8bf5717079
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-10 10:12:12 -0300

    Correcting wrong word

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -15,7 +15,7 @@ const NavBar = () =>{
             <img src={logo} alt="Logo TI" className="w-10 cursor-pointer" onClick={goToHome}/>
             <div>
                 <Link to="/" className="text-neutral-50 mx-5 hover:text-neutral-300">HOME</Link>
-                <Link to="/out" className="text-neutral-50 mx-5 hover:text-neutral-300">OUT's</Link>
+                <Link to="/out" className="text-neutral-50 mx-5 hover:text-neutral-300">OUTPUT ITEMS</Link>
                 <Link to="/create" className="text-neutral-50 mx-5 bg-orange-600 p-2 rounded-md hover:bg-orange-500">ADD</Link>
                 <Link to="/remove" className="text-neutral-50 mx-5 bg-orange-600 p-2 rounded-md hover:bg-orange-500">TAKE OUT</Link>
             </div>
